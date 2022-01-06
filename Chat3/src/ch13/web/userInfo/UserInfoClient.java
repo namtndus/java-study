6da0722064a09777be3db3857aed9076f8980072
suyeon <rnrmf0721@gmail.com>
2022-01-07 00:48:11 +0900

    dto 설계 실습

---
+++ Chat3/src/ch13/web/userInfo/UserInfoClient.java
@@ -0,0 +1,45 @@
+package ch13.web.userInfo;
+
+import ch13.domain.userinfo.UserInfo;
+import ch13.domain.userinfo.dao.UserInfoDao;
+import ch13.domain.userinfo.dao.mysql.UserInfoMySqlDao;
+import ch13.domain.userinfo.dao.oracle.UserInfoOracleDao;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class UserInfoClient {
+
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream fis = new FileInputStream("db.properties");
+
+        //페어로 파일을 읽을 수 있는 객체 ex) 키와 값을 한꺼번에 읽을 수 있는 객체
+        Properties prop = new Properties();
+        prop.load(fis);
+        String dbType = prop.getProperty("DBTYPE");
+
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserId("12345");
+        userInfo.setPassword("!@#$");
+        userInfo.setUserName("Lee");
+
+        UserInfoDao userInfoDao = null;
+
+        if( dbType.equals("ORACLE")){
+            userInfoDao = new UserInfoOracleDao();
+        }else if(dbType.equals("MYSQL")){
+            userInfoDao = new UserInfoMySqlDao();
+        }else{
+            System.out.println("db error");
+            return;
+        }
+
+
+        userInfoDao.insertUserInfo(userInfo);
+        userInfoDao.upsateUserInfo(userInfo);
+        userInfoDao.deleteUserInfo(userInfo);
+    }
+
+}
